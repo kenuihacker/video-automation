440b4684d7cfade351cce42ef4d010a0cc004e8f
kenuihacker <71199814+kenuihacker@users.noreply.github.com>
2025-02-13 17:57:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 import requests
 from gtts import gTTS
 from moviepy.editor import *
-from google.auth.transport.requests import Request
+from google_auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from pydrive.auth import GoogleAuth
